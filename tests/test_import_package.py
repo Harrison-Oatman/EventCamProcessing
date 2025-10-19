50d18de2316bb087e185e266a4a3b3c7c55e38a0
Harrison-Oatman <ho6729@princeton.edu>
2025-10-19 11:47:49 -0400

    adds sample test

---
+++ tests/test_import_package.py
@@ -0,0 +1,8 @@
+import pytest 
+
+# sample test to show we can import the package
+def test_main():
+    """ensures that package can be downloaded - delete this when we add more tests"""
+    
+    import eventcamprocessing
+    eventcamprocessing.main()
