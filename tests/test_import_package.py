e32d5adab3baf1655fb1edb2d50af5bc1d580450
Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
2025-11-10 22:24:21 -0500

    Squashed commit of the following:
    commit 13f6fb9e83194788a93f76d19511c3c68267a289
    Author: Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
    Date:   Mon Nov 10 22:23:32 2025 -0500
    
        Merge filterfuncs and move to src
    
    commit 59609432cd70bc133c2bcac0e57299368d892729
    Merge: 753d185 9808f0b
    Author: Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
    Date:   Mon Nov 10 22:20:50 2025 -0500
    
        Merge remote-tracking branch 'origin/lowpass-hotpixel-filters' into accumulate_events
    
    commit 9808f0b4945d8c1576f8590b93de42006b31f874
    Author: jv3379 <jv3379@princeton.edu>
    Date:   Mon Nov 10 21:28:08 2025 -0500
    
        Added low pass and hot pixel filters with docstrings
    
    commit 753d18544163c996fe829439cd8e4ee113fbc61d
    Merge: 025d7ad 9e19006
    Author: Harrison-Oatman <46121828+Harrison-Oatman@users.noreply.github.com>
    Date:   Mon Nov 10 20:44:25 2025 -0500
    
        Update from main
    
    commit 025d7ad3e4ea7770295dc4a2c47cf894b2a2e34e
    Author: wfahy33 <wf3963@princeton.edu>
    Date:   Mon Nov 10 18:20:28 2025 -0500
    
        Added a fifth function: opposite polarity filter
    
    commit 4efd953e2b2925dba58530cfaddc83096bd1e2b5
    Author: wfahy33 <wf3963@princeton.edu>
    Date:   Mon Nov 10 16:03:20 2025 -0500
    
        Added first function to accumulate events

--- tests/test_import_package.py
+++
@@ -1,7 +0,0 @@
-# sample test to show we can import the package
-def test_main():
-    """ensures that package can be downloaded - delete this when we add more tests"""
-
-    import eventcamprocessing
-
-    eventcamprocessing.main()
